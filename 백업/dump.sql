442b308bd50664979fa9357d73c0f856f615be46
ttlsruddlf <79757238+ttlsruddlf@users.noreply.github.com>
2021-03-15 20:51:05 +0900

    Add files via upload

---
+++ 백업/dump.sql
@@ -0,0 +1,12 @@
+BEGIN TRANSACTION;
+CREATE TABLE main(
+            user TEXT,
+            user_id TEXT,
+            money TEXT,
+            black TEXT,
+            wrong_pin TEXT,
+            accumulated TEXT
+            );
+INSERT INTO "main" VALUES('sso#9975','612954817568178176','0','no','0','0');
+INSERT INTO "main" VALUES('h.m.#6532','418698902334734336','1000','no','0','0');
+COMMIT;
